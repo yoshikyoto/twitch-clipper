d7f5a730e4d1827147bc04781c1018a2174b7a15
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2018-10-31 00:12:58 +0900

    get user id

--- src/get_user_id.rb
+++ src/get_user_id.rb
@@ -6,5 +6,7 @@ require 'net/http'
 
 config = YAML.load_file("config.yml")
 client = Twitch::Client.new(client_id: config["twitch"]["client_id"])
-result = client.get_users({login: ARGV[0]})
-puts(result.data[0].id)
+result = client.get_users({login: ARGV})
+result.data.each{ |user| 
+  puts(user.login + ":\t" + user.id)
+}
