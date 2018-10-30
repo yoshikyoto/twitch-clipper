a28ad804f4880db1dd7a44481f59e43e99ba9b64
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2018-10-31 00:17:57 +0900

    get clip url

--- src/bundle.rb
+++ src/bundle.rb
@@ -2,5 +2,13 @@ require 'twitch-api'
 require 'yaml'
 
 puts("start")
+
 config = YAML.load_file("config.yml")
-puts(config["twitch"]["client_id"])
+client = Twitch::Client.new(client_id: config["twitch"]["client_id"])
+result = client.get_clips({broadcaster_id: "104833324"})
+
+result.data.each{ |clip|
+    puts(clip.url)
+}
+
+puts("end")
