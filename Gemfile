e87a9d33e9fbdf453e8a6246cf72d8585147cc56
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2018-11-05 01:05:03 +0900

    download video

--- Gemfile
+++ Gemfile
@@ -5,5 +5,5 @@ source "https://rubygems.org"
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 gem 'twitch-api'
-gem 'twitch-clipr'
+gem 'twitch-clipr', "~> 1.0.0"
 
--- src/bundle.rb
+++ src/bundle.rb
@@ -2,27 +2,41 @@ require 'yaml'
 require 'date'
 require 'open-uri'
 require 'twitch-api'
-require 'twitch/clipr'
+require 'twitch-clipr'
 
-puts("start")
 
-now = Date.today
-a_week_ago = now - 7
+now = Time.now.strftime("%Y%m%d%H%M%S")
+puts "start: " + now
+
+today = Date.today
+a_week_ago = today - 7
 
 config = YAML.load_file("config.yml")
+download_dir = config["video"]["download_dir"] + "/" + now
+Dir.mkdir(download_dir, 0755)
+
 twitch = Twitch::Client.new(client_id: config["twitch"]["client_id"])
+
+broadcaster_name = "riotgamesjp"
+
+# ユーザー名からユーザーIDを引く
+result = twitch.get_users({login: broadcaster_name})
+user = result.data[0]
+
 # 直近1週間のclipを視聴数順に取得
 result = twitch.get_clips({
-    broadcaster_id: "104833324",
+    broadcaster_id: user.id,
     started_at: a_week_ago.rfc3339,
-    first: 10,
+    first: 1,
 })
 
+# クリップをダウンロード
 result.data.each{ |clip|
     clipr = Twitch::Clipr::Client.new()
     download_url = clipr.get(clip.url)
-    puts(clip.url)
-    puts(download_url)
+    download_path = download_dir + "/" + broadcaster_name + "-" + clip.id + ".mp4"
+    clipr.download(download_url, download_path)
+    sleep(0.1)
 }
 
 puts("end")
