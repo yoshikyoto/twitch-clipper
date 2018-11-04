04857630909d45aec7ebfcfae334900099b27d3f
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2018-11-05 01:44:18 +0900

    add broadcasters

--- src/bundle.rb
+++ src/bundle.rb
@@ -4,39 +4,70 @@ require 'open-uri'
 require 'twitch-api'
 require 'twitch-clipr'
 
-
+# 現在時刻
 now = Time.now.strftime("%Y%m%d%H%M%S")
 puts "start: " + now
 
-today = Date.today
-a_week_ago = today - 7
+# dry-runかどうか
+is_dry_run = ARGV[0] == "--dry-run"
+if is_dry_run
+  puts "dry run"
+end
+
 
 config = YAML.load_file("config.yml")
 download_dir = config["video"]["download_dir"] + "/" + now
 Dir.mkdir(download_dir, 0755)
 
+# twitch api clientの初期化
 twitch = Twitch::Client.new(client_id: config["twitch"]["client_id"])
 
 broadcaster_name = "riotgamesjp"
 
-# ユーザー名からユーザーIDを引く
-result = twitch.get_users({login: broadcaster_name})
-user = result.data[0]
+broadcasters = config["broadcasters"]
+broadcasters.each { |broadcaster_name|
+    # ユーザー名からユーザーIDを引く
+    result = twitch.get_users({login: broadcaster_name})
+    user = result.data[0]
+    puts("name: " + broadcaster_name)
+    puts("id: " + user.id)
 
-# 直近1週間のclipを視聴数順に取得
-result = twitch.get_clips({
-    broadcaster_id: user.id,
-    started_at: a_week_ago.rfc3339,
-    first: 1,
-})
+    # dry-runの場合ユーザーIDを引いて終わり
+    if is_dry_run
+        continue
+    end
 
-# クリップをダウンロード
-result.data.each{ |clip|
-    clipr = Twitch::Clipr::Client.new()
-    download_url = clipr.get(clip.url)
-    download_path = download_dir + "/" + broadcaster_name + "-" + clip.id + ".mp4"
-    clipr.download(download_url, download_path)
-    sleep(0.1)
+    # 1週間前
+    today = Date.today
+    a_week_ago = today - 7
+
+    # 直近1週間のclipを視聴数順に取得
+    result = twitch.get_clips({
+        broadcaster_id: user.id,
+        started_at: a_week_ago.rfc3339,
+        first: 3,
+    })
+
+    # クリップをダウンロード
+    clip_rank = 1
+    result.data.each{ |clip|
+        clipr = Twitch::Clipr::Client.new()
+        download_url = clipr.get(clip.url)
+        puts("downloading: " + clip.url)
+        puts("view: " + clip.view_count.to_s)
+
+        # ダウンロードURL resource/今日の日付/配信者名-ClipId.mp4
+        download_path = 
+            download_dir + "/" + 
+            broadcaster_name + "-" + 
+            clip_rank.to_s + "-" + 
+            clip.view_count.to_s + "views-"
+            clip.id + ".mp4"
+        clipr.download(download_url, download_path)
+
+        # 負荷軽減のため0.1秒sleep
+        sleep(0.1)
+    }
 }
 
 puts("end")
