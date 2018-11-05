04bd66f3b29b4856ac345afcbd1ec04ac05d4f7a
yoshikyoto <stb_yoshiyuki-s@hotmail.co.jp>
2018-11-05 09:15:59 +0900

    remove get user id batch

--- src/get_user_id.rb
+++
@@ -1,9 +0,0 @@
-require 'twitch-api'
-require 'yaml'
-
-config = YAML.load_file("config.yml")
-client = Twitch::Client.new(client_id: config["twitch"]["client_id"])
-result = client.get_users({login: ARGV})
-result.data.each{ |user| 
-  puts(user.login + ":\t" + user.id)
-}
